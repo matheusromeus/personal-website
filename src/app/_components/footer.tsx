f651b0a78d38d64d28d0131fea5552f6bc543322
kevinsourcode <kevnroy1@gmail.com>
2024-05-29 00:42:59 +0530

    fixed footer position for various screen sizes

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,7 +4,7 @@ import { EXAMPLE_PATH } from "@/lib/constants";
 export function Footer() {
   return (
     <Container>
-      <div className="mt-52 text-[17px] flex justify-between">
+      <div className="mt-52 text-[17px] flex justify-center xl:justify-end md:mb-8 lg:justify-end">
         <div className="underline hover:no-underline hover:cursor-pointer">
           {/* check out my featured works↗️ */}
         </div>
