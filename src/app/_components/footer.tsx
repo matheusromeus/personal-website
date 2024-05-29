da3e990952cef834b668455468db19e74e1e1e79
kevinsourcode <kevnroy1@gmail.com>
2024-05-29 06:44:18 +0530

    cleanin up codebase

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,13 +1,9 @@
 import Container from "@/app/_components/container";
-import { EXAMPLE_PATH } from "@/lib/constants";
 
 export function Footer() {
   return (
     <Container>
       <div className="mt-52 text-[17px] flex justify-center xl:justify-end md:mb-8 lg:justify-end">
-        <div className="underline hover:no-underline hover:cursor-pointer">
-          {/* check out my featured works↗️ */}
-        </div>
         <div>
         © Digital Bleeding Studios. 2024.
         </div>
