1bd8f4f3d9aa5135d9434a35c9fc9d29896a3811
kevinsourcode <kevnroy1@gmail.com>
2024-05-29 06:54:40 +0530

    fixing footer font size

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -3,7 +3,7 @@ import Container from "@/app/_components/container";
 export function Footer() {
   return (
     <Container>
-      <div className="mt-52 text-[17px] flex justify-center xl:justify-end md:mb-8 lg:justify-end">
+      <div className="mt-52  mb-6 text-sm xl:text-[17px] lg:text-[17px] flex justify-center xl:justify-end md:mb-8 lg:justify-end">
         <div>
         © Digital Bleeding Studios. 2024.
         </div>
