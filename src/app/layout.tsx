d45273bfc19eaef8c9eacc56353e7f32e8b6e991
kevinsourcode <kevnroy1@gmail.com>
2024-07-08 21:35:41 +0530

    adding og-image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
           type="image/png"
           sizes="32x32"
