93f6150a85173bd41773d0f3526acc40c88339cb
romeus <kevnroy1@gmail.com>
2025-06-11 12:54:46 +0530

    added back text

--- src/app/_components/another_section.tsx
+++ src/app/_components/another_section.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Another_section = () => {
-  return <section className="mt-20"></section>;
+  return (
+    <section className="mt-20">
+      <h4 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight select-none">
+        I'm a jack of all trades, which is why i chose Full stack. Tossed in a
+        tad of A.I too, cause why not? My crazy goal? To become the master of
+        all trades. A bit ambitious, but I'm all in.
+      </h4>
+      <h4 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight select-none">
+        My obsession with knowing everything, it's real. Right now, I'm neck
+        deep in low-level programming and Linux. Can't decide if I should dive
+        into Rust or Zig. Decisions, decisions.
+      </h4>
+      <h4 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight select-none">
+        Here's what I believe: the future is for the deep divers. The ones who
+        really get into the nitty gritty of tech. And that's where I'm headed.
+      </h4>
+    </section>
+  );
 };
 
 export default Another_section;
